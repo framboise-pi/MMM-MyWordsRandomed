e96ab86df8ea551b7e92a3a5918db8232a324844
framboise-pi <55726262+framboise-pi@users.noreply.github.com>
2020-06-02 06:17:57 +0200

    Create package-lock.js

---
+++ package-lock.js
@@ -0,0 +1,13 @@
+{
+  "name": "mmm-mywordsrandomed",
+  "version": "0.0.1",
+  "lockfileVersion": 1,
+  "requires": true,
+  "dependencies": {
+    "java-random": {
+      "version": "0.4.0",
+      "resolved": "https://registry.npmjs.org/java-random/-/java-random-0.4.0.tgz",
+      "integrity": "sha512-l2IZbnB877+oTytU15ac1Mp90fDrKSDpZfpDqqgCAxVjxN/HdVdM7S7YScqQ4SoBMCyjMJz+nj5h8YOereOn9Q=="
+    }
+  }
+}
